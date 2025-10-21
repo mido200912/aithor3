e8e02b0081b1ff6cb9397d32c82d31cdb2237d59
mido200912 <mohammedalsyedalsyed12@gmail.com>
2025-10-22 01:03:47 +0300

    Update ChatInterface.js

--- src/pages/ChatInterface.js
+++ src/pages/ChatInterface.js
@@ -16,7 +16,7 @@ const ChatInterface = () => {
     }
 
     try {
-      const res = await axios.get(`http://localhost:5000/api/public/company/${apiKey}`);
+      const res = await axios.get(`https://aithor-wiatp99s.b4a.run/api/public/company/${apiKey}`);
       if (res.data.success) {
         setCompany(res.data.company);
         localStorage.setItem("companyApiKey", apiKey);
@@ -43,7 +43,7 @@ const ChatInterface = () => {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/public/chat", {
+      const res = await axios.post("https://aithor-wiatp99s.b4a.run/api/public/chat", {
         companyApiKey: apiKey,
         prompt,
       });
